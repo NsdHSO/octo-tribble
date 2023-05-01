dfa1dae09ed7e069160a4677b5c40838cdf2ac30
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@gmail.com>
2023-05-01 08:43:53 +0300

    chore:🌻 added monster slayer

--- app.js
+++ app.js
@@ -38,84 +38,131 @@ Vue.createApp({
 
 Vue.createApp({
   computed: {
-    addNumberComplex(){
-      if(this.addNumber < 37)
-        return 'Not there yet'
-      else if(this.addNumber > 37)
-        return 'Too much'
-      else 
-        return this.addNumber
-    }
+    addNumberComplex() {
+      if (this.addNumber < 37) return "Not there yet";
+      else if (this.addNumber > 37) return "Too much";
+      else return this.addNumber;
+    },
   },
   watch: {
-    addNumberComplex () {
+    addNumberComplex() {
       const self = this;
       setTimeout(() => {
         self.addNumber = 0;
       }, 5000);
-     }
+    },
   },
   data() {
     return {
-      addNumber:0,
+      addNumber: 0,
     };
   },
   methods: {
-    addingNumber(enteredValue){
-      this.addNumber += enteredValue
-  }
-  }
+    addingNumber(enteredValue) {
+      this.addNumber += enteredValue;
+    },
+  },
 }).mount(".container-id");
 Vue.createApp({
-  data(){
+  data() {
     return {
       boxASelected: false,
       boxBSelected: false,
-      dataInput: '',
-      hiddenP:false,
-      styleP: ''
-    }
+      dataInput: "",
+      hiddenP: false,
+      styleP: "",
+    };
   },
-  computed:{
-      dataInputs(){
-        console.log(
-          this.dataInput
-        );
-        if(this.dataInput === 'user1')
-          return 'bg-danger'
-        else{
-          return 'bg-warning'
-        }
+  computed: {
+    dataInputs() {
+      console.log(this.dataInput);
+      if (this.dataInput === "user1") return "bg-danger";
+      else {
+        return "bg-warning";
       }
+    },
   },
   methods: {
-    boxSelected (event){
-        if(event ==='A')
-          this.boxASelected = !this.boxASelected;
-        else if( event === 'B'){
-          this.boxBSelected = !this.boxBSelected;
-        }
+    boxSelected(event) {
+      if (event === "A") this.boxASelected = !this.boxASelected;
+      else if (event === "B") {
+        this.boxBSelected = !this.boxBSelected;
+      }
     },
-    toggleState(){
-      this.hiddenP = !this.hiddenP
-    }
-  }
-
-}).mount('.screen_content')
+    toggleState() {
+      this.hiddenP = !this.hiddenP;
+    },
+  },
+}).mount(".screen_content");
 
 Vue.createApp({
-  data(){
+  data() {
     return {
       goals: [],
-      goal:''
-    }
+      goal: "",
+    };
   },
   methods: {
-    addNewGoal(){
-      if(this.goal !== ''){
+    addNewGoal() {
+      if (this.goal !== "") {
         this.goals.push(this.goal);
-        this.goal = ''
+        this.goal = "";
       }
-    }
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
   },
-}).mount('.screen')
+}).mount(".screen");
+
+Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      life: [
+        { check: true, label: "Life" },
+        { check: true, label: "Life" },
+        { check: true, label: "Life" },
+      ],
+    };
+  },
+  methods: {
+    randomNumber(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    },
+    attackMonster(min, max) {
+      this.monsterHealth -= this.randomNumber(min, max);
+    },
+    playerAttack() {
+      this.playerHealth -= this.randomNumber(
+        this.randomNumber(5, 5),
+        this.randomNumber(5, 18)
+      );
+      this.attackMonster(5, 12);
+    },
+    specialAttack() {
+      this.playerHealth -= this.randomNumber(
+        this.randomNumber(5, 5),
+        this.randomNumber(15, 28)
+      );
+      this.attackMonster(
+        this.randomNumber(this.randomNumber(5, 5), this.randomNumber(7, 8)),
+        this.randomNumber(this.randomNumber(15, 15), this.randomNumber(25, 38))
+      );
+    },
+    addedHeal(index) {
+      if (this.playerHealth < 100 && this.monsterHealth < 100) {
+        let fountIndex = this.life.find(
+          (element, indexElement) => indexElement === index
+        );
+        if (fountIndex.check !== false) {
+          fountIndex.label = "Used";
+          fountIndex.check = false;
+          this.playerHealth += 20;
+          this.monsterHealth += 20;
+        }
+      }
+    },
+  },
+}).mount(".continer-game");
